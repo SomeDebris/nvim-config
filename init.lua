10a973a8d72cb10b0376a37925e34f0998d905ea
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2025-09-18 08:56:58 -0700

    add a default awk-language-server config so that it works

--- init.lua
+++ init.lua
@@ -267,7 +267,21 @@ local lsps = {
             settings = {}
         }, --]=]
     },
-    -- { "awk-language-server" },
+    { "awk-language-server",
+        {
+            cmd = { 'awk-language-server' },
+            filetypes = { 'awk' },
+            single_file_support = true,
+            handlers = {
+                ['workspace/workspaceFolders'] = function()
+                    return {{
+                        uri = 'file://' .. vim.fn.getcwd(),
+                        name = 'current_dir',
+                    }}
+                end
+            }
+        }
+    },
     {
         "clangd",
         {
