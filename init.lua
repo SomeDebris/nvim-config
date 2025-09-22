b0fe45a493633e4a743c75e638f8f7f85ee1aeab
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2025-09-21 17:29:29 -0700

    init.lua: add vimtex

--- init.lua
+++ init.lua
@@ -189,6 +189,15 @@ require("lazy").setup({
       'JuliaEditorSupport/julia-vim'
     },
     {
+      "lervag/vimtex",
+      lazy = false,     -- we don't want to lazy load VimTeX
+      -- tag = "v2.15", -- uncomment to pin to a specific release
+      init = function()
+        -- VimTeX configuration goes here, e.g.
+        vim.g.vimtex_view_method = "sioyek"
+      end
+    },
+    {
       'nvim-treesitter/nvim-treesitter',
       lazy = false,
       branch = 'main',
